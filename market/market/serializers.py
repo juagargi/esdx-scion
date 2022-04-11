45b502fd7e1c431db370594ec33795f15267b02a
Juan A. Garcia Pardo <juagargi@gmail.com>
2022-04-11 11:31:01 +0200

    fix grpc generation

--- market/market/serializers.py
+++ market/market/serializers.py
@@ -1,10 +1,10 @@
 from market.models import Offer
 from django_grpc_framework import proto_serializers
-import market.market_pb2
+import market_pb2
 
 
 class OfferProtoSerializer(proto_serializers.ModelProtoSerializer):
     class Meta:
         model = Offer
-        proto_class = market.market_pb2.Offer
+        proto_class = market_pb2.Offer
         fields = ['ia_id']
--- market/tools/make_protobuf.sh
+++ market/tools/make_protobuf.sh
@@ -4,5 +4,5 @@ BASE=$(dirname "$0")
 
 
 # python -m grpc_tools.protoc --proto_path=./ --python_out=./ --grpc_python_out=./ ./account.proto
-cd "${BASE}/../market/"
-python -m grpc_tools.protoc --proto_path=../proto/ --python_out=./ --grpc_python_out=./ ../proto/market.proto
+cd "${BASE}/../"
+python -m grpc_tools.protoc --proto_path=./proto/ --python_out=./ --grpc_python_out=./ ./proto/market.proto
